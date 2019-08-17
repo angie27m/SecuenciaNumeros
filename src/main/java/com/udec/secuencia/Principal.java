a461ae43c8656e4f11053aaffa0e5067672e9a76
Manrique <angie27manrique@gmail.com>
2019-08-16 19:56:57 -0500

    commit inicial

---
+++ src/main/java/com/udec/secuencia/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udec.secuencia;
+
+/**
+ *
+ * @author angie
+ */
+public class Principal {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*int cores = Runtime.getRuntime().availableProcessors();
+        System.out.print(cores);*/
+        Secuencia sec = new Secuencia();
+    }
+}
